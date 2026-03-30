1d045f8dd462d08283cd8535c59c11ac2f6a3c26
Fatimags13 <fatigar@okstate.edu>
2026-03-30 16:00:40 -0500

    Add Loop class with constructor and attributes

---
+++ Loop6_1.py
@@ -0,0 +1,14 @@
+# LOOP.PY
+# region class definitions
+class Loop():
+    #region constructor
+    def __init__(self):
+        """
+        Defines a loop as a list of node names.
+        """
+        #region attributes
+        self.Nodes = []
+        self.Name = ''
+        #endregion
+    #endregion
+#endregion
